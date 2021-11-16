e07f6fdc58becb1f20e88ea46fbd13e09431546f
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-16 11:28:26 -0600

    format menu & menuData with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Menu, Footer } from './components/index'
-import { About, Portfolio, Resume, Contact } from './pages/index' 
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Menu, Footer } from "./components/index";
+import { About, Portfolio, Resume, Contact } from "./pages/index";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Menu />
-       <Switch>
+      <Switch>
         <Route exact path="/about" component={About} />
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/resume" component={Resume} />
         <Route path="/contact" component={Contact} />
-       </Switch>
+      </Switch>
       <Footer />
     </Router>
   );
--- src/components/MenuData.jsx
+++ src/components/MenuData.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import * as BsIcons from 'react-icons/bs'
-import * as GrIcons from 'react-icons/gr'
-import * as MdIcons from 'react-icons/md'
+import React from "react";
+import * as BsIcons from "react-icons/bs";
+import * as GrIcons from "react-icons/gr";
+import * as MdIcons from "react-icons/md";
 
 export const MenuData = [
-    {
-        title: 'About',
-        path: '/about',
-        icon: <BsIcons.BsInfoCircle />,
-        cName: 'menu-text',
-    },
-    {
-        title: 'Portfolio',
-        path: '/portfolio',
-        icon: <GrIcons.GrProjects />,
-        cName: 'menu-text',
-    },
-    {
-        title: 'Resume',
-        path: '/resume',
-        icon: <MdIcons.MdOutlineFolderShared />, 
-        cName: 'menu-text',
-    },
-    {
-        title: 'Contact',
-        path: '/contact',
-        icon: <GrIcons.GrContactInfo />,
-        cName: 'menu-text',
-    }
-]
+  {
+    title: "About",
+    path: "/about",
+    icon: <BsIcons.BsInfoCircle />,
+    cName: "menu-text",
+  },
+  {
+    title: "Portfolio",
+    path: "/portfolio",
+    icon: <GrIcons.GrProjects />,
+    cName: "menu-text",
+  },
+  {
+    title: "Resume",
+    path: "/resume",
+    icon: <MdIcons.MdOutlineFolderShared />,
+    cName: "menu-text",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    icon: <GrIcons.GrContactInfo />,
+    cName: "menu-text",
+  },
+];
