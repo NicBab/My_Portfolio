15e1ff0c622c2b56a5bf42cddec1dc092a14689a
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-11-18 10:31:40 -0600

    added contactPg info and css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './css/Contact.css'
+import { phoneIcon, mailIcon } from '../Icons'
 
 const Contact = () => {
+    const onContactLinkedInClick = () => {
+        window.open("https://www.linkedin.com/in/nic-babineaux/")
+      }
+    
+      const onContactGitHubClick = () => {
+        window.open("https://github.com/NicBab")
+      }
+    
+      const onContactPhoneClick = () => {
+        window.location = "tel:+3372579202"
+      }
+    
+      const onContactEmailClick = () => {
+        window.location.href="mailto: nickpbab@gmail.com"
+      }
+
     return (
-        <div className="contact">
-            Contact Page
+       <>
+        <div className="contactPg">
+          <div className="contactLinkedIn" onClick={onContactLinkedInClick}>LinkedIn</div>
+          <div className="contactGitHub" onClick={onContactGitHubClick}>GitHub</div>
+          <div className="contactEmail" onClick={onContactEmailClick}>{mailIcon}nickpbab@gmail.com</div>
+          <div className="contactPhone" onClick={onContactPhoneClick}>{phoneIcon}(337)257-9202</div>
         </div>
+      </>
     )
 }
 
